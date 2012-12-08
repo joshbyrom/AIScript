92a296ee39bed1201b4ad3ed0be95b3985bccf19
joshbyrom <joshbyrom@gmail.com>
2012-12-07 23:54:50 -0500

    Lindenmayer systems added

--- js/AIDevelopmentalSystems.js
+++ js/AIDevelopmentalSystems.js
@@ -1,8 +1,27 @@
 ﻿AIScript.modules.DevelopmentalSystems = function (aiScript, modules) {
+    this.Alphabet = function () {
+        this.symbols = Array.prototype.slice.call(arguments);
+        this.symbols = (this.symbols[0] && Array.isArray(this.symbols[0]) ? this.symbols[0] : this.symbols);
+    };
+
+    this.Alphabet.prototype.contains = function (symbol) {
+        return this.symbols.indexOf(symbol) in this.symbols;
+    };
+
+    this.Axiom = function (string) {
+        this.string = string;
+    };
+
     this.ProductionRule = function (predecessor, successor) {
         this.predecessor = predecessor;
         this.successor = successor;
-    }
+    };
 
-
+    this.ProductionRule.prototype.apply = function (symbol) {
+        if (this.predecessor === symbol) {
+            return this.successor;
+        } else {
+            return false;
+        }
+    };
 };
